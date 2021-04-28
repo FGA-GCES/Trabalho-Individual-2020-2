1c373cc1b8eaa5fe98987558ca0f17db07baf7ff
lorryaze <lorrany90@gmail.com>
2021-04-28 16:45:36 -0300

    codecov start

---
+++ api/test_api.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+pip install coverage
+
+coverage run /api/manage.py test
+
+RESULT=$?
+if [ ${RESULT} != "0" ]; then
+	echo -e "\nOh no, some test failed!"
+	exit ${RESULT}
+fi
+
+echo -e "\nGreat, all tests succeed!\n"
+
+coverage report -m
+
+#codecov -t cfb96bda-6e0b-4788-9bf6-ee85a8feb4aa
+
+exit 0
