25398b619523ecaae6373bc013a3dc02ff37b620
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-30 16:40:40 -0300

    change env variable name for api url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import axios from 'axios';
 
-console.log(process.env.API_URL)
+console.log(process.env.VUE_APP_API_URL)
 export const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:8000"
+  baseURL: process.env.VUE_APP_API_URL || "http://localhost:8000"
 })
