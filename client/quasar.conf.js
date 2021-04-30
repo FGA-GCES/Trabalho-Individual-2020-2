7446128ec2362c648bbb879d11045ac45fb1b040
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-30 15:49:38 -0300

    trying to make deploy on client to work on heroku

--- client/quasar.conf.js
+++ client/quasar.conf.js
@@ -74,7 +74,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: process.env('PORT') || 8080,
+      port: process.env.PORT || 8080,
       open: true // opens browser window automatically
     },
 
