39ff9911573a7870dcb38a3703d566f50ff18508
joao vitor silva <joaovitordmrosa@hotmail.com>
2021-04-29 22:58:21 -0300

    Add containerization

--- api/api/settings.py
+++ api/api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'myproject',
         'USER': 'myuser',
         'PASSWORD': 'password',
-        'HOST': 'localhost',
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
         'PORT': '',
     }
 }
