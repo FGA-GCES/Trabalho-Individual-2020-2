9d1d2225560d7c6e3e697f3685d1d5405bcd1d14
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-01 13:48:30 -0300

    fix typo to get NODE_ENV

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const prodUrl = 'https://trabalho-gces-api.herokuapp.com';
 const devUrl = "http://localhost:8000";
 
-const url = process.env.NOE_ENV === 'development' ? devUrl : prodUrl;
+const url = process.env.NODE_ENV === 'development' ? devUrl : prodUrl;
 
 export const api = axios.create({
   baseURL: url
