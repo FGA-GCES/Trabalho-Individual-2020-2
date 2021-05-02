9c8db4b2fabf02e6404c57175563eb6785fa71f5
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2021-05-02 19:58:39 -0300

    CI

--- api/task/tests.py
+++ api/task/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from task.models import Task
 class TestModels(TestCase):
     def test_task_creation(self):
-        task_title = "Task title"
+        task_title = "Task_title"
         task_description = "Task description"
         task = Task.objects.create(title=task_title, description=task_description)
         self.assertEqual(task.title, task_title)
