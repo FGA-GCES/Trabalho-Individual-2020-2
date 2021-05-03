300f818f33d7eaa1b89806ac87282fde45afcab2
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2021-05-03 19:37:16 -0300

    testing climate

--- api/task/models.py
+++ api/task/models.py
@@ -4,6 +4,11 @@ class Task(models.Model):
     title = models.CharField("Title", max_length=240)
     description = models.CharField("Title", max_length=240)
     created = models.DateField(auto_now_add=True)
-
+    if(1):
+        if(1):
+            if(1):
+                if(1):
+                    if(1):
+                        print("kaboom")
     def __str__(self):
         return self.name
