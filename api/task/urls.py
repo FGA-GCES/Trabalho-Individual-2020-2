dd56af8fe31c0d29eed0f5b8b803d047e6a93254
Guilherme Deusdará <guilherme.banci@somosdx.co>
2021-05-04 15:45:31 -0300

    ajusting python files to pass pyflake
    Signed-off-by: Guilherme Deusdará <guilherme.banci@somosdx.co>

--- api/task/urls.py
+++ api/task/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import path
 from .views import TaskCreate, TaskList, TaskDetail, TaskUpdate, TaskDelete
 
 urlpatterns = [
--- api/task/views.py
+++ api/task/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import Task
 from rest_framework import generics
 from .serializers import TaskSerializer
