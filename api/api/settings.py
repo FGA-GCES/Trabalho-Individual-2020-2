50719c5313fe6c90f425cfafaab5752002791db2
Guilherme Deusdará <guilherme.banci@somosdx.co>
2021-05-04 01:51:00 -0300

    adicionando .env com todas as variaveis ambientee
    Signed-off-by: Guilherme Deusdará <guilherme.banci@somosdx.co>

--- api/api/settings.py
+++ api/api/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
