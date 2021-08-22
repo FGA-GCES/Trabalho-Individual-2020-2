aa230e641e3ecc58be1d1bb962e3d070010bb70b
Gustavo Lima <gustavomarques.gml@gmail.com>
2021-08-21 21:43:54 -0300

    Test fail 2

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,3 +1,3 @@
-docker-compose build
-docker-compose --file test-api.yml up --exit-code-from api
-docker-compose --file test-client.yml up --exit-code-from client
+docker-compose build || exit 1
+docker-compose --file test-api.yml up --exit-code-from api || exit 1
+docker-compose --file test-client.yml up --exit-code-from client || exit 1
